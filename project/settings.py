8688f723c9cd5abe78165ab9bd24073155e8dbb7
takiyalu <lucas.takiya@gmail.com>
2024-08-23 13:19:00 -0300

    heroku

--- project/settings.py
+++ project/settings.py
@@ -92,19 +92,11 @@ DATABASES = {
 }
 """
 # Using PostgreSQL for HEROKU
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config()
-    }
-else:
-    # Local development settings
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
-        }
-    }
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
